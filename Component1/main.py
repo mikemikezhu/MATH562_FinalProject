844b7c342d41d1e5811da534c65632a0d92d8658
grosjuice <villeneuve_99@hotmail.com>
2026-03-21 16:46:36 -0400

    Testing for experiment 1

--- Component1/main.py
+++ Component1/main.py
@@ -2,6 +2,7 @@
 Imports 
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def generate_data(n, d, cov_a, sigma2, beta_n):
@@ -30,7 +31,7 @@ def compute_true_risk(theta_star, theta, cov_a, sigma2, beta_n):
     return 1/2 * (theta_star - theta).T @ cov_a @ (theta_star - theta) + 1/2 * sigma2 * beta_n
 
 
-def run_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs):
+def run_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma):
 
     empirical_risk_hist = []
     true_risk_hist = []
@@ -40,7 +41,6 @@ def run_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs):
     A, b, theta_star = generate_data(n, d, cov_a, sigma2, beta_n)
 
     theta = np.random.randn(d) # Random initialization. TODO : Maybe change this
-    gamma = 1/d # Learning TODO : Optimize the constant
 
     for epoch in range(num_epochs):
         for _ in range(n):
@@ -53,3 +53,46 @@ def run_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs):
         true_risk_hist.append(compute_true_risk(theta_star, theta, cov_a, sigma2, beta_n))
 
     return empirical_risk_hist, true_risk_hist
+
+
+"""
+Experiment 1
+"""
+rho = 1
+d_list = [100, 200, 400, 800, 1600]
+num_runs = 10
+all_runs_empirical_risk = []
+all_runs_true_risk = []
+num_epochs = 100
+
+
+
+# Testing
+d = 100
+cov_a = np.eye(d) # TODO: Change alpha_d
+sigma2 = 1
+beta_n =1
+gamma = 1/d
+
+for run in range(num_runs): # TODO : wrap in d 
+    empirical_risk_hist, true_risk_hist = run_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma)
+    all_runs_empirical_risk.append(empirical_risk_hist)
+    all_runs_true_risk.append(true_risk_hist)
+
+
+all_runs_empirical_risk = np.array(all_runs_empirical_risk)
+all_runs_true_risk = np.array(all_runs_true_risk)
+
+empirical_risk_hist_mean = np.mean(all_runs_empirical_risk, axis=0)
+empirical_risk_hist_std = np.std(all_runs_empirical_risk, axis=0)
+
+plt.plot(np.arange(num_epochs), empirical_risk_hist_mean)
+plt.fill_between(np.arange(num_epochs), 
+                 empirical_risk_hist_mean - empirical_risk_hist_std, 
+                 empirical_risk_hist_mean + empirical_risk_hist_std, 
+                 alpha=0.2)
+plt.xlabel("Num of epochs")
+plt.ylabel("Empirical risk")
+plt.title("Training error")
+plt.yscale("log")
+plt.show()

23aa019354df6616f08a8657de630f4e37dbf70b
MAAzzouz <107534022+MAAzzouz@users.noreply.github.com>
2026-04-03 06:13:43 -0400

    Completed Code for Component 1, Experiment 4 + Optional Experiment

--- component1/main.py
+++ component1/main.py
@@ -288,7 +288,7 @@ def single_suffle_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma, step_typ
     return empirical_risk_hist, true_risk_hist
 
 
-def multiple_suffle_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma, step_type):
+def multiple_shuffle_sgd(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma, step_type):
 
     empirical_risk_hist = []
     true_risk_hist = []
@@ -415,3 +415,55 @@ def sgd_batch(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma, step_type, batch
             theta -= (gamma(d) / L) * grad
 
     return empirical_risk_hist, true_risk_hist
+
+###############################################################################
+# Optional experiment - Repeating experiment 4 but with SGD with momentum
+###############################################################################
+
+def sgd_momentum_batch(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma, step_type, batch_size, delta):
+
+    empirical_risk_hist = []
+    true_risk_hist = []
+
+    n = int(d/rho)
+
+    A, b, theta_star = generate_data(n, d, cov_a, sigma2, beta_n)
+
+    theta = np.random.randn(d) # Random initialization
+    theta_prev = theta.copy()
+
+    # Set absolute constant L (for the learning rate gamma(d))
+    U, S, Vh = np.linalg.svd(A) # To find the singular values of A^T A (and thus its eigenvalues)
+    if step_type == "max":
+        L = S[0]**2
+    
+    elif step_type == "avg":
+        L = np.mean(S**2)
+
+    else:
+        L = 1
+
+    for epoch in range(num_epochs):
+
+        empirical_risk_hist.append(compute_empirical_risk(A, b, theta))
+        true_risk_hist.append(compute_true_risk(theta_star, theta, cov_a, sigma2, beta_n))
+
+        for _ in range(int(n/batch_size(n))):
+            idx  = np.random.choice(n, batch_size(n), replace=False) # Batch
+            a_idx = A[idx, :]
+            b_idx = b[idx]
+            theta_old = theta.copy()
+
+            grad = (a_idx.T @ (a_idx @ theta - b_idx))* 1/n / (batch_size(n) * n) + delta * (theta - theta_prev)
+            theta -= (gamma(d) / L)  * grad
+            theta_prev = theta_old
+
+    return empirical_risk_hist, true_risk_hist
+
+def sgd_momentum_batch_fixed_delta(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma, step_type, batch_size):
+    """
+    Wrapper function
+    """
+    return sgd_momentum_batch(rho, d, cov_a, sigma2, beta_n, num_epochs, gamma, step_type, batch_size, delta=0.5)
+
+
